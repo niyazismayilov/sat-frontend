86bba91496893e3e4d3637bb563d8682835294ed
esgerhuseynli <huseynli.asger.project@gmail.com>
2025-03-10 14:48:28 +0400

    jj
    jj

--- src/views/dashboard/manager/courses/edit/edit-course.tsx
+++ src/views/dashboard/manager/courses/edit/edit-course.tsx
@@ -239,8 +239,12 @@ export const EditCourse: React.FC = () => {
                                                             İdarəetmə
                                                         </MenuItem>
                                                         <MenuItem value={Enum_Course_Category.Sales}>Satış</MenuItem>
-                                                        <MenuItem value={Enum_Course_Category.Marketing}>Marketinq</MenuItem>
-                                                        <MenuItem value={Enum_Course_Category.Satacademy}>SAT Academy</MenuItem>
+                                                        <MenuItem value={Enum_Course_Category.Marketing}>
+                                                            Marketinq
+                                                        </MenuItem>
+                                                        <MenuItem value={Enum_Course_Category.Satacademy}>
+                                                            SAT Academy
+                                                        </MenuItem>
                                                         <MenuItem value={Enum_Course_Category.Nartio}>Nartio</MenuItem>
                                                         <MenuItem value={Enum_Course_Category.Others}>Digər</MenuItem>
                                                     </Field>
