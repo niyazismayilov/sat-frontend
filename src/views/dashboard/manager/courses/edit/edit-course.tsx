1b887db1b913aec1a222e5d829dc6296468a985b
esgerhuseynli <huseynli.asger.project@gmail.com>
2025-03-10 14:51:32 +0400

    hh
    hh

--- src/views/dashboard/manager/courses/edit/edit-course.tsx
+++ src/views/dashboard/manager/courses/edit/edit-course.tsx
@@ -245,7 +245,6 @@ export const EditCourse: React.FC = () => {
                                                         <MenuItem value={Enum_Course_Category.Satacademy}>
                                                             SAT Academy
                                                         </MenuItem>
-                                                        <MenuItem value={Enum_Course_Category.Nartio}>Nartio</MenuItem>
                                                         <MenuItem value={Enum_Course_Category.Others}>Digər</MenuItem>
                                                     </Field>
                                                 </Grid>
