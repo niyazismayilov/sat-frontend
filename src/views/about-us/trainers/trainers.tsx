aa7b1043767b4a778ba7b26d2d5adb1bc5d30e36
esgerhuseynli <huseynli.asger.project@gmail.com>
2025-03-11 12:46:57 +0400

    ss
    ss

--- src/views/about-us/trainers/trainers.tsx
+++ src/views/about-us/trainers/trainers.tsx
@@ -52,6 +52,7 @@ export const Trainers = () => {
     const { t } = useTranslation();
 
     const [count, setCount] = useState(6);
+    export const trainerUrl = "some-url";
 
     const trainers = [
         {
