fb2bcd6de5a950526fad231a132c590d63fc050e
esgerhuseynli <huseynli.asger.project@gmail.com>
2025-03-25 14:44:33 +0400

    video 8
    video 8

--- src/views/video-detail/video-detail.tsx
+++ src/views/video-detail/video-detail.tsx
@@ -74,15 +74,19 @@ export const VideoDetail = () => {
     return (
         <Root>
             <Container>
-                <Box className="video-url" sx={{ position: 'relative', width: '100%', paddingTop: '56.25%' }}>
-                    <iframe
-                        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                        src={`https://www.youtube.com/embed/${videoUrlGetID(broadcast?.videoId)}`}
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
+                <Box>
+                    {video.videoId && (
+                        <iframe
+                            width="100%"
+                            height="300"
+                            src={`https://www.youtube.com/embed/${videoUrlGetID(video?.videoId)}`}
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                            title="Embedded youtube"
+                            className="video-url"
+                        />
+                    )}
                 </Box>
 
                 <Box className="video-detail-categories">
